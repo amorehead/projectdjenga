a3606080ebb99132414eaed6f0a1148731644f22
Alex Morehead <alex.morehead@gmail.com>
2017-10-13 23:48:29 -0500

    Added a new blog app, created a generic view for the contact and blog pages, and also implemented an admin user account along with admin blog controls.

--- projectdjenga/urls.py
+++ projectdjenga/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', include('webapp.urls')),
+    url(r'^', include('webapp.urls')),
+    url(r'^blog/', include('blog.urls')),
 ]
--- webapp/views.py
+++ webapp/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def index(request):
     return render(request, 'webapp/home.html')
+
+def contact(request):
+    return render(request, 'webapp/basic.html',{'content':['If you would like to contact us, please visit our Github repository page with the buttons below.']})
