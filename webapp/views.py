1cad9d0f645c59e5ab57ed3fc86ac93fa96a896e
devOpElliot <efortney@missouriwestern.edu>
2017-11-16 15:37:38 -0600

    stickied menu to top

--- webapp/views.py
+++ webapp/views.py
@@ -5,4 +5,4 @@ def index(request):
     return render(request, 'webapp/home.html')
 
 def contact(request):
-    return render(request, 'webapp/basic.html',{'content':['If you would like to contact us, please visit our Github repository page with the buttons below.']})
+    return render(request, 'webapp/contact.html',{'content':['If you would like to contact us, please visit our Github repository page with the buttons below.']})
